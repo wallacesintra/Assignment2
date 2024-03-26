cf478cbc11a9dbdecd23ff28fc1a89c0c6d62702
wallacesintra <wallvxwallace962@gmail.com>
2024-03-26 19:41:41 +0300

    Fix setCookie function to include SameSite and Secure attributes

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ function getCookie(name) {
 }
 
 function setCookie(name, value, days) {
-  let expires = "";
-  if (days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
+    let expires = "";
+    if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; SameSite=None; Secure; path=/";
   }
-  document.cookie = name + "=" + value + expires + "; path=/";
-}
 
 function greetVisitor() {
   const visitorName = getCookie('visitorName');
